b554e773d503d101828394c7b913b4b1a2d3ac49
Domenico Di Iorio <ddiiorio@jmawireless.com>
2020-07-12 01:20:19 +0200

    Added Post unit test

---
+++ src/lib-core/test/Post_test.cpp
@@ -0,0 +1,27 @@
+#include <ctime>
+using std::time;
+using std::time_t;
+#include <memory>
+using std::shared_ptr;
+
+#include <Post.hpp>
+using ddi::kata::Post;
+
+#include <CppUTest/TestHarness.h>
+
+TEST_GROUP(PostGroup)
+{
+};
+
+TEST(PostGroup, constructor)
+{
+    Post post("Hey there!");
+    const time_t now = time(nullptr);
+    CHECK_EQUAL("Hey there!", post.content());
+    /**
+     it's quite unlikely to take more than one second 
+     between post construction and 'now' initialization
+     */
+    CHECK(now <= post.created());
+    CHECK(now > (post.created() - 1));
+}
