daf66d79a3e614614ca921b5826091dafa352471
Domenico Di Iorio <ddiiorio@jmawireless.com>
2020-07-12 00:52:49 +0200

    updated install-dependencies.sh

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,3 +1,3 @@
-yum install -y cpputest-3.8-0.x86_64.rpm
-rpm -ql cpputest | grep cpputest.pc
-export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig/
+yum install -y cpputest-3.8-0.x86_64.rpm autoconf automake make
+pkg_path=$(rpm -ql cpputest | grep cpputest.pc | sed 's/cpputest.pc//')
+echo "export PKG_CONFIG_PATH=$pkg_path"
