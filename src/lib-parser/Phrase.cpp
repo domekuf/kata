96cace9bbc1330729d7a0425c0dffae2bed436ee
Domenico Di Iorio <ddiiorio@jmawireless.com>
2020-07-12 13:00:42 +0200

    * master-* master: Implemented Phrase class in lib-parser

--- src/lib-parser/Phrase.cpp
+++ src/lib-parser/Phrase.cpp
@@ -1,3 +1,9 @@
+#include <map>
+using std::map;
+#include <regex>
+using std::regex;
+using std::regex_match;
+using std::smatch;
 #include <string>
 using std::string;
 #include <boost/optional.hpp>
@@ -7,8 +13,49 @@ using boost::optional;
 namespace ddi {
 namespace kata {
 
-Phrase::Phrase(const string& /* command */)
+const map<string, Phrase::verb> Phrase::verb_map_ = {
+    {"",        READ},
+    {"follows", FOLLOW},
+    {"->",      POST},
+    {"wall",    WALL}
+};
+
+Phrase::Phrase(const string& command)
 {
+    regex re("(.*)( +(->|follows|wall) +?(.*)?)|(.*)", regex::extended);
+    //        ^--^|  ^---------------^   ^--^ | ^--^
+    //            ^---------------------------^
+    //        1      3                   4      5
+    // $1: subject
+    // $3: predicate
+    // $4: direct object
+    // $5: subject when no verb
+    smatch sm;
+    if (regex_match(command, sm, re)) {
+        unsigned c = 0;
+        for (const auto s : sm) {
+            switch (c) {
+                case 1:
+                subject_ = s;
+                break;
+                case 3:
+                if (verb_map_.find(s) != verb_map_.end())
+                    predicate_ = verb_map_.at(s);
+                break;
+                case 4:
+                if ("" != s)
+                    object_ = s;
+                break;
+                case 5:
+                if ("" != s)
+                    subject_ = s;
+                break;
+                default:
+                break;
+            }
+            c ++;
+        }
+    }
 }
 
 optional<string> Phrase::object() const
--- src/lib-parser/Phrase.hpp
+++ src/lib-parser/Phrase.hpp
@@ -1,5 +1,6 @@
 #ifndef SRC_LIB_PARSER_PHRASE_HPP_
 #define SRC_LIB_PARSER_PHRASE_HPP_
+#include <map>
 #include <string>
 #include <boost/optional.hpp>
 namespace ddi {
@@ -16,20 +17,33 @@ public:
     } verb;
     /**
      * @brief Construct a new Phrase object
-     * 
+     *
      * @param command               command that will be parsed 
      */
     Phrase(const std::string& command);
 
     /**
      * @brief get phrase direct object
-     * 
-     * @return boost::optional<std::string> 
+     *
+     * @return boost::optional<std::string> direct object 
      */
     boost::optional<std::string>    object() const;
+
+    /**
+     * @brief get phrase predicate
+     *
+     * @return verb                 predicate
+     */
     verb                            predicate() const;
+
+    /**
+     * @brief get phrase subject
+     *
+     * @return std::string          subject
+     */
     std::string                     subject() const;
 private:
+    static const std::map<std::string, verb> verb_map_;
     boost::optional<std::string>    object_;
     verb                            predicate_;
     std::string                     subject_;
