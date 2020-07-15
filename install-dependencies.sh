a192f972d69b0d0a42a750cba1b8c252e2726c6a
Domenico Di Iorio <dome.diiorio@gmail.com>
2020-07-15 23:40:55 +0200

    Updated dependencies

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,3 +1,13 @@
-yum install -y cpputest-3.8-0.x86_64.rpm autoconf automake make
+yum install -y \
+    autoconf \
+    automake \
+    make \
+    cpputest-3.8-0.x86_64.rpm \
+    odb \
+    boost-devel \
+    libodb-devel \
+    libdob-sqlite-devel \
+    libodb-boost-devel \
+    sqlite-devel
 pkg_path=$(rpm -ql cpputest | grep cpputest.pc | sed 's/cpputest.pc//')
 echo "export PKG_CONFIG_PATH=$pkg_path"
